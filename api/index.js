8f53545e5d029833f00eb9931f150ad719bad121
Mateus9349 <mateusvpdl@outlook.com>
2023-07-12 14:20:09 -0400

    liberando entradas https

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express');
 const routes = require('./routes')
 
@@ -7,6 +10,13 @@ const port = 4000;
 
 routes(app)
 
-app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`))
+app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`));
+
+https.createServer({
+    cert: fs.readFileSync('api/SSL/fullchain.pem'),
+    key: fs.readFileSync('api/SSL/privkey.pem')
+}, app).listen(4001, ()=> console.log('Rodando em https'));
+
+
 
 module.exports = app

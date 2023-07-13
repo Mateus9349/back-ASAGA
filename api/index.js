02b2347c46737f272bebbbe800166fb620c10882
Mateus9349 <mateusvpdl@outlook.com>
2023-07-13 16:36:33 -0400

    mudando a porta para 443

--- api/index.js
+++ api/index.js
@@ -14,8 +14,9 @@ app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`));
 
 https.createServer({
     cert: fs.readFileSync('api/SSL/certificate.crt'),
-    key: fs.readFileSync('api/SSL/private.key')
-}, app).listen(4001, ()=> console.log('Rodando em https'));
+    key: fs.readFileSync('api/SSL/private.key'),
+    ca: fs.readFileSync('api/SSL/ca_bundle.crt')
+}, app).listen(443, ()=> console.log('Rodando em https'));
 
 
 
